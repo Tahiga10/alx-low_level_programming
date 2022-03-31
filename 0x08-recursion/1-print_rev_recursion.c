d3dfc038fe208687007bf6f8a77eb3c70a2fa143
Tahiga10 <yoroevrard@gmail.com>
2022-03-31 10:37:46 -0700

    task 1 debbuging

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- *  * *_strlen_recursion - returns the length of a string.
- *   * @s: string to to counted;
- *    *
- *     * Return: return (0)
+ *  * *_print_rev_recursion - prints a string in reverse
+ *   * @s: string to be printed
+ *    * Return: returns 0
  */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
